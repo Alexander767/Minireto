a4fac301198d487bf0672b9b73905a53477f4481
Alexander767 <alejandro767@gmail.com>
2015-11-13 14:43:13 -0600

    Archivo mini reto playground

---
+++ Minireto v2.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: A01129407 Alejandro Alvarez mini reto
+
+import UIKit
+
+for lista in 1...100 {
+    
+    if (( lista >= 30) && ( lista <= 40 ) && (lista % 5 == 0) && ( lista % 3 == 0 ))
+    { print ("#\(lista) Viva Swift, Bingo!!!  y es impar")
+        
+    }else if (( lista >= 30) && ( lista <= 40 ) && (lista % 5 == 0) && ( lista % 3 != 0 ))
+        { print ("#\(lista) Viva Swift, Bingo!!!  y es impar")
+        
+    }else if (( lista >= 30) && ( lista <= 40 ) && (lista % 5 == 0) && ( lista % 2 == 0 ))
+    { print ("#\(lista) Viva Swift, Bingo!!!  y es par")
+        
+    }else if (( lista >= 30) && ( lista <= 40 ) && (lista % 2 == 0))
+    { print ("#\(lista) Viva Swift  y es par")
+        
+    }else if (( lista >= 30) && ( lista <= 40 ) && (lista % 3 != 0))
+    { print ("#\(lista) Viva Swift  y es impar")
+        
+    }else if (( lista >= 30) && ( lista <= 40 ) && (lista % 3 == 0))
+    { print ("#\(lista) Viva Swift  y es impar")
+    
+    }else if (( lista % 5 == 0) && ( lista % 3 != 0 ) && ( lista  % 2  != 0 ))
+        
+    {print ("#\(lista) Bingo!!! y es impar")
+        
+    }else if (( lista % 5 == 0 ) && (lista % 2 == 0 ))
+        
+    {print ("#\(lista) Bingo!!! y es par")
+        
+    }else if lista % 2 == 0 {
+        
+        print("#\(lista) par")
+        
+    }else
+    {print ("#\(lista) impar")
+}
+
+}
+
+
+
