bd9a5f92b84e2660abad81c9e76359be89fdeeb6
Alexander767 <alejandro767@gmail.com>
2015-11-13 20:51:10 -0600

    Mini Reto
    Ya corregido con el 0

--- Minireto v2.playground/Contents.swift
+++ Minireto v2.playground/Contents.swift
@@ -2,9 +2,15 @@
 
 import UIKit
 
-for lista in 1...100 {
+for lista in 0...100 {
     
-    if (( lista >= 30) && ( lista <= 40 ) && (lista % 5 == 0) && ( lista % 3 == 0 ))
+    
+    if lista < 1 {
+        
+        print("#\(lista) ")
+        
+    }else if (( lista >= 30) && ( lista <= 40 ) && (lista % 5 == 0) && ( lista % 3 == 0 ))
+        
     { print ("#\(lista) Viva Swift, Bingo!!!  y es impar")
         
     }else if (( lista >= 30) && ( lista <= 40 ) && (lista % 5 == 0) && ( lista % 3 != 0 ))
@@ -30,12 +36,15 @@ for lista in 1...100 {
         
     {print ("#\(lista) Bingo!!! y es par")
         
+        
+  
     }else if lista % 2 == 0 {
         
         print("#\(lista) par")
         
     }else
     {print ("#\(lista) impar")
+        
 }
 
 }
